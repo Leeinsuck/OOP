c41740290567d4ad8d68da8796a4a619c4ba6733
Leeinsuck <remon0906@naver.com>
2022-05-24 23:11:00 +0900

    0524

---
+++ 0524/0524메모장원본/0524메모장원본/Form2.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace _0524메모장원본
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Find = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 9F);
+            this.label1.Location = new System.Drawing.Point(40, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "찾을 내용(&N):";
+            // 
+            // txt_Find
+            // 
+            this.txt_Find.Location = new System.Drawing.Point(166, 45);
+            this.txt_Find.Multiline = true;
+            this.txt_Find.Name = "txt_Find";
+            this.txt_Find.Size = new System.Drawing.Size(331, 30);
+            this.txt_Find.TabIndex = 1;
+            this.txt_Find.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "다음 찾기(&F)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "취소";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(43, 103);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(180, 22);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "대/소문자 구분(&C)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(229, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 52);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "방향";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "위로(&U)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(105, 24);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "아래로(&D)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 154);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Find);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "찾기";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Find;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
---
+++ 0524/0524메모장원본/0524메모장원본/Form2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0524메모장원본
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private Form1 dnn = null;
+
+        public Form2(Form1 objDotnetNote)
+        {
+            dnn = objDotnetNote;
+            InitializeComponent();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!FindText())
+            {
+                MessageBox.Show(
+                    this.txt_Find.Text + "을(를) 찾을 수 없습니다."
+                    , "메모장"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Information
+                    );
+            }
+        }
+        private bool FindText()
+        {
+            int nFind;
+            int nLen;
+            string strTempText;
+            string strTempFind;
+
+            if (checkBox1.Checked)
+            {
+                strTempText = dnn.textBox1.Text; //찾을 대상
+                strTempFind = txt_Find.Text; //찾을 단어
+            }
+            else
+            {
+                strTempText = dnn.textBox1.Text.ToLower(); //소문자
+                strTempFind = txt_Find.Text.ToLower(); //소문자
+            }
+
+            nLen = txt_Find.Text.Length; //텍스트 길이
+
+            //위로 / 아래로 검색
+            if (radioButton1.Checked)
+            {
+                if ((dnn.textBox1.SelectionStart - dnn.textBox1.SelectionLength) < 0)
+                    nFind = -1;
+                else
+                    nFind = strTempText.LastIndexOf(
+                        strTempFind
+                        , dnn.textBox1.SelectionStart
+                          - dnn.textBox1.SelectionLength
+                          );
+            }
+            else // 아래로
+            {
+                nFind = strTempText.IndexOf(
+                    strTempFind
+                    , dnn.textBox1.SelectionStart
+                      + dnn.textBox1.SelectionLength);
+
+            }
+
+            if (nFind == -1)
+                return false;
+            else
+            {
+                dnn.textBox1.SelectionStart = nFind;
+                dnn.textBox1.SelectionLength = nLen;
+                dnn.textBox1.Focus();
+                return true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.button1.Enabled = true;
+        }
+    }
+}
