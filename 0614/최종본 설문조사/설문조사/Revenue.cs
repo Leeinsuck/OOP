581d95005d12b25fcf63fffc5e5e3371873b00e1
Leeinsuck <remon0906@naver.com>
2022-06-14 23:27:00 +0900

    0614

---
+++ 0614/최종본 설문조사/설문조사/Revenue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 설문조사
+{
+    public class Revenue
+    {
+        public int 선택 { get; set; }
+        public int 학생수 { get; set; }
+
+
+    }
+}
